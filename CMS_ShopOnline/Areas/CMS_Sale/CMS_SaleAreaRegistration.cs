299624904c0a6c81cf32c0adf3ccf6c19a159d25
TranTheVinh-CDTH17PMC <56820186+TranTheVinh-CDTH17PMC@users.noreply.github.com>
2020-05-04 13:35:01 +0700

    TranTheVinh-CDTH17PMC/DoAnOnline

---
+++ CMS_ShopOnline/Areas/CMS_Sale/CMS_SaleAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace CMS_ShopOnline.Areas.CMS_Sale
+{
+    public class CMS_SaleAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "CMS_Sale";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "CMS_Sale_default",
+                "CMS_Sale/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
