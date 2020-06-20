a69407b9f1f91dc8520bc6b27b924bc9af494f31
NguyenHuyenThoai-CDTH17PMC <56819507+NguyenHuyenThoai-CDTH17PMC@users.noreply.github.com>
2020-06-20 12:34:27 +0700

    ...

---
+++ CMS_ShopOnline/Areas/CMS_Sale/Controllers/InOrderController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CMS_ShopOnline.Areas.CMS_Sale.Controllers
+{
+    public class InOrderController : Controller
+    {
+        //
+        // GET: /CMS_Sale/InOrder/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
